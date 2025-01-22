031575e6f91dbf414b496c7121a5490831220f29
Sally Schaffner <schaffners@vpcc.edu>
2025-01-22 14:32:51 -0500

    Started Planet program

--- Examples 1-22-2025.cpp
+++ Examples 1-22-2025.cpp
@@ -1,22 +1,18 @@
-// This program uses an array of ten characters to store the
-// first ten letters of the alphabet. The ASCII codes of the
-// characters are displayed.
-
-#include <iostream>
-//using namespace std;
+// This program initializes a string array.
+#include<iostream>
+#include<string>
+using namespace std;
 
 int main()
 {
-    const int NUM_LETTERS = 10;
-    char letters[NUM_LETTERS] = { 'A', 'B', 'C', 'D', 'E',
-                                 'F', 'G', 'H', 'I', 'J' };
+    const int SIZE = 9;
+    string planets[SIZE] = { "Mercury", "Venus", "Earth", "Mars",
+                             "Jupiter", "Saturn", "Uranus",
+                             "Neptune", "Pluto (a dwarf planet)" };
 
-    std::cout << "Character" << "\t" << "ASCII Code\n";
-    std::cout << "---------" << "\t" << "----------\n";
-    for (int count = 0; count < NUM_LETTERS; count++)
-    {
-        std::cout << letters[count] << "\t\t";
-        std::cout << static_cast<int>(letters[count]) << std::endl;
-    }
+    cout << "Here are the planets:\n";
+
+    for (int count = 0; count < SIZE; count++)
+        cout << planets[count] << endl;
     return 0;
 }
